c9f9be4eda23ed67bbe2345692694c58548bd79c
Astory <dracomalfoy233@163.com>
2018-02-02 21:37:45 +0800

    修改了get和post

--- server/app/json/activity.py
+++ server/app/json/activity.py
@@ -1,3 +1,4 @@
+import json
 from app import db, models
 from flask_restful import Resource, Api, reqparse, abort
 
@@ -11,26 +12,41 @@ parse.add_argument('picture')
 class activity(Resource):
     # 查询活动信息
     def get(self, id):
-        activity = models.activity.query.get(id)
-        # 判断活动是否存在
-        if activity:
-            return {
-                       "id": activity.id,
-                       "name": activity.name,
-                       "introduce": activity.introduce,
-                       "picture": activity.picture,
-                   }, 200
+        # 若id为-1，返回整个列表
+        if id==-1:
+            activities = models.activity.query.all() #返回的activities是一个列表，其中每个元素都是一个 activity 类型的对象
+            d = {}
+            d["activity"] = []
+            for activity in activities:
+                dic = {}
+                dic["id"] = activity.id
+                dic["name"] = activity.name
+                dic["introduce"] = activity.introduce
+                dic["picture"] = activity.picture
+                item = json.dumps(dic)
+                #print(item)
+                d["activity"].append(item)
+            json_str = json.dumps(d)
+            return json_str
         else:
-            return {
-                abort(404, message="{} doesn't exist".format(id))
-            }
+            activity = models.activity.query.get(id)
+            # 判断活动是否存在
+            if activity:
+                return {
+                    "id": activity.id,
+                    "name": activity.name,
+                    "introduce": activity.introduce,
+                    "picture": activity.picture,
+                }, 200
+            else:
+                return {
+                    abort(404, message="{} doesn't exist".format(id))
+                }
 
     # 添加活动信息
-    def post(self, id):
-        # 判断活动是否存在
-        if models.activity.query.get(id):
-            abort(400, message="{} existed".format(id))
-        # 创建活动
+    def post(self):
+        max = models.activity.query.order_by(db.desc(models.activity.id)).first()
+        id = max.id+1 if max else 1
         activity = models.activity()
         activity.id = id
         args = parse.parse_args()
