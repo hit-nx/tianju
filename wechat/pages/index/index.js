84364b3e8ad398e8039e1f94126ff72d3fba1146
Mrguan12 <2306192334@qq.com>
2018-02-02 17:10:04 +0800

    首页
    初始版本，有很多地方还要修改。

--- wechat/pages/index/index.js
+++ wechat/pages/index/index.js
@@ -1,73 +1,16 @@
-//index.js
-//获取应用实例
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    firstbtn:"分享给好友",
+    secondbtn:"取消参与活动",
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  },
-
-  searchRestaurant: function () {
-      wx.navigateTo({
-          url: '../searchRestaurant/searchRestaurant',
-      })
-  }
+  
 })
