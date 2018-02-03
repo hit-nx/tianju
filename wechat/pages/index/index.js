74c95b11430824e4f7bb891e617990a92dca6301
Mrguan12 <2306192334@qq.com>
2018-02-03 14:42:13 +0800

    首页
    不知道怎么了，clone下来之后没有了，又重新添加的。

--- wechat/pages/index/index.js
+++ wechat/pages/index/index.js
@@ -6,7 +6,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    firstbtn: "分享给好友",
+    secondbtn: "取消参与活动",
   },
 
   /**
