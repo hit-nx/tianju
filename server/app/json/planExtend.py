27d2efd1e4d6e13ad87aedb8bc32c0b60c0028d5
randylltarly <1695215206@qq.com>
2018-02-03 20:14:00 +0800

    py

--- server/app/json/planExtend.py
+++ server/app/json/planExtend.py
@@ -3,20 +3,26 @@ from flask_restful import Resource, Api, reqparse, abort
 
 # 参数初始化
 parse = reqparse.RequestParser()
+parse.add_argument("wechat")
+parse.add_argument("plan")
 parse.add_argument('extend')
 
 
 class planExtend(Resource):
     # 修改
-    def put(self, wechat):
-        planExtend = models.planExtend.query.get(wechat)
+    def put(self):
         args = parse.parse_args()
+        plan=models.plan.query.filter_by(args.plan).first()
+        planExtend = models.planExtend.query.filter_by(args.extend).first()
         # 判断用户是否存在
-        if planExtend:
-            planExtend.extend = args.extend if args.extend else planExtend.extend
-            db.session.commit()
-            return {"message": True}
+        if planExtend and plan:
+            if args.wechat==plan.wechat:
+                planExtend.extend = args.extend if args.extend else planExtend.extend
+                db.session.commit()
+                return {"message": True}
+            else:
+                return {"message":"please call the source"}
         else:
             return {
-                abort(404, message="{} doesn't exist".format(wechat))
+                abort(404, message="{} doesn't exist".format(plan.wechat))
             }
--- server/app/json/planHotel.py
+++ server/app/json/planHotel.py
@@ -3,20 +3,25 @@ from flask_restful import Resource, Api, reqparse, abort
 
 # 参数初始化
 parse = reqparse.RequestParser()
+parse.add_argument("wechat")
+parse.add_argument("id")
 parse.add_argument('hotel')
 
 
 class planHotel(Resource):
     # 修改
-    def put(self, wechat):
-        plan = models.plan.query.get(wechat)
+    def put(self):
         args = parse.parse_args()
+        plan = models.plan.query.filter_by(id=args.id).first()
         # 判断用户是否存在
         if plan:
-            plan.hotel = args.grade if args.grade else plan.hotel
-            db.session.commit()
-            return {"message": True}
+            if args.wechat==plan.wechat:
+                plan.hotel = args.hotel if args.hotel else plan.hotel
+                db.session.commit()
+                return {"message": True}
+            else:
+                return {"message":"please call the source"}
         else:
             return {
-                abort(404, message="{} doesn't exist".format(wechat))
+                abort(404, message="{} doesn't exist".format(plan.wechat))
             }
