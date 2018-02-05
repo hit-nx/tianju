9e5c6b1e5be13a590483f7f5e89cf8fe0fb1c583
Mrguan12 <2306192334@qq.com>
2018-02-05 22:10:36 +0800

    新建活动按钮点击事件
    基本完成

--- wechat/pages/newActivity/newActivity.js
+++ wechat/pages/newActivity/newActivity.js
@@ -4,11 +4,37 @@ Page({
    * 页面的初始数据
    */
   data: {
+    array:[{
+      name:'匆匆那年',
+      choose:'../../images/icon-png/选择(未选中).png',
+    }, {
+      name: '民俗',
+      choose: '../../images/icon-png/选择(未选中).png',
+    }, {
+      name: '海洋之旅',
+      choose: '../../images/icon-png/选择(未选中).png',
+    }, {
+      name: '联谊会',
+      choose: '../../images/icon-png/选择(未选中).png',
+    }, {
+      name: '旅游',
+      choose: '../../images/icon-png/选择(未选中).png',
+    }, ],
     showModal: false,
     showView: 'none',
     photoUrl: '../../images/icon-png/右箭头.png',
+    height:'0%',
+    camera:'../../images/icon-png/选择(未选中).png',
+    cloth: '../../images/icon-png/选择(未选中).png'
   },
+  /**
+   * onLoad
+   */
   onLoad: function () {
+    var up=(this.data.array.length+2)*5.2+'%'
+    this.setData({
+      height:up
+    })
   },
   /**
    * 弹窗
@@ -53,5 +79,90 @@ Page({
       photoUrl:photo,
       showView:show
     })
+  },
+  /**
+   * 选择活动方案
+   */
+  choose:function (e) {
+    var num=this.data.array.length-1
+    for(;num>=0;num--){
+      var up = 'array['+num+'].choose'
+      this.setData({
+        [up]: '../../images/icon-png/选择(未选中).png'
+      })
+    } 
+   
+    var up = 'array[' + e.currentTarget.id + '].choose'
+    this.setData({
+      [up]: '../../images/icon-png/选择(选中).png'
+    })
+  },
+  /**
+   * 查看活动方案
+   */
+  look:function (e) {
+    console.log(e.currentTarget.id)
+  },
+  /**
+   * 点击住宿地点
+   */
+  hotelShow:function () {
+    wx.navigateTo({
+      url: '../hotelShow/hotelShow',
+    })
+  },
+  /**
+   * 点击用餐地点
+   */
+  selectRestaurant:function(){
+    wx.navigateTo({
+      url: '../selectRestaurant/selectRestaurant',
+    })
+  },
+  /**
+   * 点击纪念品选择
+   */
+  addsouvenir:function(){
+    wx.navigateTo({
+      url: '../addsouvenir/addsouvenir',
+    })
+  },
+  /**
+   * 点击个人需求
+   */
+  individualDemand:function(){
+    wx.navigateTo({
+      url: '../individualDemand/individualDemand',
+    })
+  },
+  /**
+   * 点击航拍需求
+   */
+  camera:function() {
+    if (this.data.camera =='../../images/icon-png/选择(未选中).png'){
+      this.setData({
+        camera: '../../images/icon-png/确认.png'
+      })
+    }
+    else{
+      this.setData({
+        camera: '../../images/icon-png/选择(未选中).png'
+      })
+    }
+  },
+  /**
+   * 点击集体定制服装需求
+   */
+  cloth: function () {
+    if (this.data.cloth == '../../images/icon-png/选择(未选中).png') {
+      this.setData({
+        cloth: '../../images/icon-png/确认.png'
+      })
+    }
+    else {
+      this.setData({
+        cloth: '../../images/icon-png/选择(未选中).png'
+      })
+    }
   }
 })
