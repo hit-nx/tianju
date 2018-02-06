0f6fe9c1c1544b03e247244ff0bcee3ec9d39099
Mrguan12 <2306192334@qq.com>
2018-02-06 14:10:47 +0800

    酒店展示
    动态化处理

--- wechat/pages/hotelShow/hotelShow.js
+++ wechat/pages/hotelShow/hotelShow.js
@@ -4,7 +4,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    array:[{
+      style:'photo',
+      photo:'../../images/photos/酒店.jpg',
+      name:'我是酒店名称',
+      location:'我是酒店地点',
+      choose:'../../images/icon-png/选择(未选中).png'
+    }, {
+      style: 'photo',
+      photo: '../../images/photos/酒店.jpg',
+      name: '我是酒店名称',
+      location: '我是酒店地点',
+      choose: '../../images/icon-png/选择(未选中).png'
+    }, {
+      style: 'photo',
+      photo: '../../images/photos/酒店.jpg',
+      name: '我是酒店名称',
+      location: '我是酒店地点',
+      choose: '../../images/icon-png/选择(未选中).png'
+    }, {
+      style: 'photo',
+      photo: '../../images/photos/酒店.jpg',
+      name: '我是酒店名称',
+      location: '我是酒店地点',
+      choose: '../../images/icon-png/选择(未选中).png'
+    }, {
+      style: 'nophoto',
+      photo: '../../images/icon-png/酒店搜索+添加餐厅(无图片).png',
+      name: '我是酒店名称',
+      location: '我是酒店地点',
+      choose: '../../images/icon-png/选择(未选中).png'
+    },]
   },
+  /**
+   * 点击确认icon
+   */
+  choose: function(e){
+    var num = this.data.array.length - 1
+    for (; num >= 0; num--) {
+      var up = 'array[' + num + '].choose'
+      this.setData({
+        [up]: '../../images/icon-png/选择(未选中).png'
+      })
+    }
 
+    var up = 'array[' + e.currentTarget.id + '].choose'
+    this.setData({
+      [up]: '../../images/icon-png/确认.png'
+    })
+  }
 })
