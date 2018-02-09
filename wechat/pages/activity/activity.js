cd5c40b512abaec8dcd1d082c0ca5fe1cef3fc55
Mrguan12 <2306192334@qq.com>
2018-02-09 23:51:53 +0800

    解决冲突
    解决冲突

--- wechat/pages/activity/activity.js
+++ wechat/pages/activity/activity.js
@@ -71,10 +71,12 @@ Page({
       url: 'http://47.94.99.203:5000/plan?id=1',
       header:{
         'Content-Type': 'json'
-      }d:'GET',
+      },
+      method:'GET',
       success:function(res){
-        that.setData{
-          post:res.data[0]
+        that.setData({
+          post: res.data[0]
+        })   
       },
       fail:function(){
         console.log("qwert")
