14ae878ae992e790ed268d98bd05ce3ec4dbb5cc
randylltarly <1695215206@qq.com>
2018-02-13 11:26:40 +0800

    kkk

--- wechat/pages/activity/activity.js
+++ wechat/pages/activity/activity.js
@@ -1,64 +1,75 @@
 Page({
 
+  
   /**
    * 页面的初始数据
    */
   data: {
-    firstTime:'2017-8-10',
-    secondTime:'2017-8-11',
+    firstTime:'',
+    secondTime:'',
 
     firstDay:[{
       act:'上午',
-      title:'参观校园',
-      add:'地点：校训石',
-      Else:'备注：无备注'
+      title:'',
+      add:'',
+      Else:'',
+      id:0
     }, {
       act: '午餐',
-      title: '学子餐厅午餐',
-      add: '地点：学子餐厅四楼',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     }, {
       act: '中午',
-      title: '午间休息',
-      add: '地点：酒店',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     }, {
       act: '下午',
-      title: '班会',
-      add: '地点：大学生活动中心',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     }, {
       act: '晚餐',
-      title: '谢师宴',
-      add: '地点：自定',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     },],
 
     secondDay: [{
       act: '上午',
-      title: '自由活动',
-      add: '地点：自定',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     }, {
       act: '午餐',
-      title: '午餐',
-      add: '地点：自定',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     }, {
       act: '中午',
-      title: '午间休息',
-      add: '地点：酒店',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     }, {
       act: '下午',
-      title: '步行环海(重走军训路)',
-      add: '地点：环海路',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     }, {
       act: '晚餐',
-      title: '晚餐',
-      add: '地点：自定',
-      Else: '备注：无备注'
+      title: '',
+      add: '',
+      Else: '',
+      id: 0
     },]
   },
 
@@ -67,24 +78,192 @@ Page({
    */
   onLoad: function (options) { 
     var that=this;
+    var change='';
+    var Day=''
+    console.log(options.click+"     "+options.activity)
+    
+    //console.log(options.plan_id)
+    that.setData({
+      wechat:options.wechat,
+      plan_id:options.plan_id
+    })
+    function timestampToTime(timestamp) {
+      var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+      var Y = date.getFullYear() + '-';
+      var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+      var D = (date.getDate()<10?'0'+(date.getDate()):date.getDate()) + ' ';
+     
+      return Y + M + D;
+    }
     wx.request({
-      url: 'http://47.94.99.203:5000/plan?id=1',
+      url: 'http://127.0.0.1:5000/plan?id='+options.plan_id,
       header:{
         'Content-Type': 'json'
-      }d:'GET',
+      },
+      method:'GET',
       success:function(res){
-        that.setData{
-          post:res.data[0]
+        wx.setNavigationBarTitle({
+          title: res.data.name
+        })
+        var time1= timestampToTime(res.data.date)
+        var time2=timestampToTime(res.data.date+86400)
+        that.setData({
+          firstTime:time1,
+          secondTime:time2
+        })
+        var a = new Array();
+        a[0] = options.click == 0 ? options.activity : res.data.activity_one
+        a[1] = options.click == 2 ? options.activity : res.data.activity_two
+        a[2] = options.click == 3 ? options.activity : res.data.activity_three
+        a[3] = options.click == 5 ? options.activity : res.data.activity_four
+        a[4] = options.click == 7 ? options.activity : res.data.activity_five
+        a[5] = options.click == 8 ? options.activity : res.data.activity_six
+        var b = new Array();
+        b[0] = 0
+        b[1] = 2
+        b[2] = 3
+        b[3] = 0
+        b[4] = 2
+        b[5] = 3
+        var c = new Array();
+        c[0] = options.click == 1 ? options.activity : res.data.restaurant_one
+        c[1] = options.click == 4 ? options.activity : res.data.restaurant_two
+        c[2] = options.click == 6 ? options.activity : res.data.restaurant_three
+        c[3] = options.click == 9 ? options.activity : res.data.restaurant_four
+        var d=new Array();
+        d[0] = 1
+        d[1] = 4
+        d[2] = 1
+        d[3] = 4
+        //获取活动
+        wx.request({
+          url: 'http://127.0.0.1:5000/activity/0',
+          header:{
+            'Content-Type':'json'
+          },
+          method:'GET',
+          success:function(res1){
+            for(var key=0;key<6;++key){
+              for (var i = 0; i < res1.data.activity.length; ++i) {
+                if(key<3){
+                  Day='firstDay['
+                }
+                else{
+                  Day='secondDay['
+                }
+                if (a[key] == null) {
+                  change = Day + b[key] +'].title'
+                  that.setData({
+                    [change]: "未选择"
+                  })
+                  change = Day + b[key] +'].add'
+                  that.setData({
+                    [change]: ''
+                  })
+                  change = Day + b[key] +'].Else'
+                  that.setData({
+                    [change]: ''
+                  })
+                  change = Day + b[key] + '].id'
+                  that.setData({
+                    [change]: 0
+                  })
+                  break;
+                }
+                if (a[key] == res1.data.activity[i].id) {
+                  change = Day + b[key] +'].title'
+                  that.setData({
+                    [change]: res1.data.activity[i].name
+                  })
+                  change = Day + b[key] +'].add'
+                  that.setData({
+                    [change]: ''
+                  })
+                  change = Day + b[key] +'].Else'
+                  that.setData({
+                    [change]: "备注:" + res1.data.activity[i].introduce
+                  })
+                  change = Day + b[key] + '].id'
+                  that.setData({
+                    [change]: a[key]
+                  })
+                  break;
+                }
+              }
+            }
+            
+          }
+          
+        })
+        //获取餐厅
+        wx.request({
+          url: 'http://127.0.0.1:5000/restaurant/0',
+          header:{
+            'Content-Type':'json'
+          },
+          method:'GET',
+          success:function(res1){
+            for (var key = 0; key < 4; ++key) {
+              for (var i = 0; i < res1.data.restaurant.length; ++i) {
+                if (key < 2) {
+                  Day = 'firstDay['
+                }
+                else {
+                  Day = 'secondDay['
+                }
+                if (c[key] == null) {
+                  change = Day + d[key] + '].title'
+                  that.setData({
+                    [change]: "未选择"
+                  })
+                  change = Day + d[key] + '].add'
+                  that.setData({
+                    [change]: ''
+                  })
+                  change = Day + d[key] + '].Else'
+                  that.setData({
+                    [change]: ''
+                  })
+                  change = Day + d[key] + '].id'
+                  that.setData({
+                    [change]: 0
+                  })
+                  break;
+                }
+                if (c[key] == res1.data.restaurant[i].id) {
+                  change = Day + d[key] + '].title'
+                  that.setData({
+                    [change]: res1.data.restaurant[i].name
+                  })
+                  change = Day + d[key] + '].add'
+                  that.setData({
+                    [change]: ''
+                  })
+                  change = Day + d[key] + '].Else'
+                  that.setData({
+                    [change]: "备注:" + res1.data.restaurant[i].introduce
+                  })
+                  change = Day + d[key] + '].id'
+                  that.setData({
+                    [change]: a[key]
+                  })
+                  break;
+                }
+              }
+            }
+          }
+        })
+        
       },
       fail:function(){
-        console.log("qwert")
         //fail
       },
       complete:function(){
-        console.log("complete")
         //complete
       }
+
     })
+    
   },
 
   /**
@@ -134,5 +313,41 @@ Page({
    */
   onShareAppMessage: function () {
     
+  },
+
+  //点击到这个活动
+  onclick:function(e){
+    var kind=e.currentTarget.id;
+    var that=this;
+    //console.log(kind)
+    wx.navigateTo({
+      url: '../activityDisplay/activityDisplay?click='+kind+'&plan_id='+that.data.plan_id+'&wechat='+that.data.wechat,
+    })
+  },
+  save:function(e){
+    var that=this
+    console.log(that.data.plan_id)
+    wx.request({
+      url: 'http://127.0.0.1:5000/plan/activity',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      data:{
+        id:that.data.plan_id,
+        wechat:that.data.wechat,
+        activity_one: that.data.firstDay[0].id,
+        activity_two: that.data.firstDay[2].id,
+        activity_three: that.data.firstDay[3].id,
+        activity_four: that.data.secondDay[0].id,
+        activity_five: that.data.secondDay[2].id,
+        activity_six: that.data.secondDay[3].id
+      },
+      method: 'PUT',
+      success:function(res){
+        console.log(res.data.message)
+        console.log("成功!!")
+        //提示保存成功
+      }
+    })
   }
 })
--- wechat/pages/activityDisplay/activityDisplay.js
+++ wechat/pages/activityDisplay/activityDisplay.js
@@ -40,14 +40,73 @@ Page({
       address: "我是活动地址",
       remarks: "我是活动备注",
       decideIcon: '../../images/icon-png/选择(未选中).png'
-    }],   
+    }],
+    decideadd:''   
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var activityList=[];
+    var that = this;
+    var ta=options.click;
+    that.setData({
+      plan_id:options.plan_id,
+      wechat:options.wechat,
+      click:options.click
+    })
+    //console.log(ta)
+    //console.log(this.data.array[0].name)
+    var resname;
+    if(ta==1){
+      console.log("66666666666666666666666666666")
+    }
+    if(ta=='0'||ta=='2'||ta=='3'||ta=='5'||ta=='7'||ta=='8'){
+      //console.log("activity")
+      wx.request({
+        url: 'http://127.0.0.1:5000/activity/0',
+        header: {
+          'Content-Type': 'json'
+        },
+        method: 'GET',
+        success: function (res) {
+          for (var key in res.data.activity) {
+            res.data.activity[key]["decideIcon"] = '../../images/icon-png/选择(未选中).png'
+            res.data.activity[key]["decideId"] = String(res.data.activity[key].id) + 'l' + String(key)
+          }
+          that.setData({
+            post: res.data.activity
+          })
+          //this.data["p"]=res.data.activity
+          //console.log(res.data.activity)
+          //console.log(that.data.post)
+        }
+      })
+    }
+    else{
+      //console.log("restaurant")
+      wx.request({
+        url: 'http://127.0.0.1:5000/restaurant/0',
+        header: {
+          'Content-Type': 'json'
+        },
+        method: 'GET',
+        success: function (res) {
+          for (var key in res.data.restaurant) {
+            res.data.restaurant[key]["decideIcon"] = '../../images/icon-png/选择(未选中).png'
+            res.data.restaurant[key]["decideId"] = String(res.data.restaurant[key].id) + 'l' + String(key)
+          }
+          that.setData({
+            post: res.data.restaurant
+          })
+          //this.data["p"]=res.data.activity
+          //console.log(res.data.activity)
+          //console.log(that.data.post)
+        }
+      })
+    }
+    
   },
 
   /**
@@ -102,16 +161,52 @@ Page({
    * 点击选择
    */
   choose:function (e) {
-    var up = "array[" + e.currentTarget.id + "].decideIcon";
-    if (this.data.array[e.currentTarget.id].decideIcon == '../../images/icon-png/选择(未选中).png'){
-      this.setData({
-        [up]:'../../images/icon-png/确认.png',
-      })
+    var that=this;
+    var kind=e.currentTarget.id;
+    console.log(kind)
+    var str=kind.split("l")
+    // console.log(str[0])//id
+    // console.log(str[1])//key
+    var up='post['+str[1]+'].decideIcon';
+    var down;
+    //console.log(that.data.post[1].decideIcon)
+    if(kind=='add'){
+      if(that.data.decideadd==''){
+        //提示请选择
+        console.log("?????????????")
+      }
+      else{
+        wx.navigateTo({
+          url: '../activity/activity?activity=' + that.data.decideadd+'&plan_id='+that.data.plan_id+'&wechat='+that.data.wechat+'&click='+that.data.click,
+        })
+        //console.log(that.data.decideadd)
+      }
     }
     else{
-      this.setData({
-        [up]: '../../images/icon-png/选择(未选中).png',
-      })
+      if (that.data.post[str[1]].decideIcon == '../../images/icon-png/确认.png') {
+        that.setData({
+          [up]: '../../images/icon-png/选择(未选中).png',
+          decideadd: ''
+        })
+      }
+      else {
+        for (var key in that.data.post) {
+          if (that.data.post[key].decideIcon == '../../images/icon-png/确认.png') {
+            down = 'post[' + key + '].decideIcon'
+            that.setData({
+              [down]: '../../images/icon-png/选择(未选中).png',
+            })
+          }
+        }
+        that.setData({
+          [up]: '../../images/icon-png/确认.png',
+        })
+        //str[0]就是当前点击的活动id
+        that.setData({
+          decideadd: str[0]
+        })
+      }
     }
+    
   }
 })
